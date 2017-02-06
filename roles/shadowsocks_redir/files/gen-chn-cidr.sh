b5675d85964cea7cb3cb5351da2b823a4dbe8829
Zhong Jianxin <azuwis@gmail.com>
2017-02-06 14:09:43 +0800

    Add custom chn cidr

--- roles/shadowsocks_redir/files/gen-chn-cidr.sh
+++ roles/shadowsocks_redir/files/gen-chn-cidr.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("            %s/%d,\n", $4, 32-log($5)/log(2)) }' > chn-cidr
+cat <<EOF >> chn-cidr
+            64.62.200.2/32,
+            1.1.1.0/24,
+EOF
