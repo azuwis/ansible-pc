2b045f341cb48f6b0314c8c6c48f7b21c5bc274e
Zhong Jianxin <azuwis@gmail.com>
2018-01-09 16:24:52 +0800

    Add files for motion viewer

---
+++ roles/motion/files/viewer/app.js
@@ -0,0 +1,58 @@
+var motion_prefix = 'motion/';
+
+Vue.component('flat-pickr', VueFlatpickr);
+
+var app = new Vue({
+    el: '#app',
+    template: '#app-template',
+    data: {
+        videos: [],
+        date: 'today'
+    },
+    computed: {
+        videosFiltered: function() {
+            var vm = this;
+            return this.videos.filter(function(video) {
+                var date = new Date(vm.date + ' 00:00:00');
+                return video.date >= date && video.date <= date.fp_incr(1);
+            });
+        },
+        dateConfig: function() {
+            var minDate = 'today';
+            var maxDate = 'today';
+            if(this.videos.length > 0) {
+                minDate = this.videos[0].date;
+                maxDate = this.videos[this.videos.length - 1].date;
+            }
+            return {
+                minDate: minDate,
+                maxDate: maxDate
+            };
+        }
+    },
+    methods: {
+        update: function() {
+            var vm = this;
+            axios.get(motion_prefix)
+                .then(function(response) {
+                    vm.videos = response.data
+                        .filter(function(file) {
+                            return file.name.endsWith('.jpg');
+                        })
+                        .map(function(file) {
+                            var m= file.name.match(/^(\d{4})-(\d{2})(\d{2})-(\d{2})(\d{2})(\d{2})\./);
+                            var date = new Date(m[1]+'-'+m[2]+'-'+m[3]+' '+m[4]+':'+m[5]+':'+m[6]);
+                            return {
+                                source: motion_prefix + file.name.replace(/\.jpg$/, '.mp4'),
+                                poster: motion_prefix + file.name,
+                                date: date,
+                                hover: false
+                            };
+                        });
+                });
+        }
+    },
+    created: function() {
+        this.update();
+    }
+});
