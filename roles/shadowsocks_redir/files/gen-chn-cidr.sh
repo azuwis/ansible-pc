749290011296b489098f14171d4bd8efb4cd2501
Zhong Jianxin <azuwis@gmail.com>
2018-01-30 14:24:35 +0800

    Aggregate chn-cidr

--- roles/shadowsocks_redir/files/gen-chn-cidr.sh
+++ roles/shadowsocks_redir/files/gen-chn-cidr.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
-curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("            %s/%d,\n", $4, 32-log($5)/log(2)) }' > chn-cidr
-cat <<EOF >> chn-cidr
-            64.62.200.2/32,
-            1.1.1.0/24,
+(
+cat <<EOF
+0.0.0.0/8
+10.0.0.0/8
+100.64.0.0/10
+127.0.0.0/8
+169.254.0.0/16
+172.16.0.0/12
+192.0.0.0/24
+192.0.2.0/24
+192.88.99.0/24
+192.168.0.0/16
+198.18.0.0/15
+198.51.100.0/24
+203.0.113.0/24
+224.0.0.0/4
+240.0.0.0/4
+255.255.255.255/32
+64.62.200.2/32
+1.1.1.0/24
 EOF
+curl -s 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }'
+) | sort -V | aggregate | sed -e 's/^/            /' -e 's/$/,/' > chn-cidr
