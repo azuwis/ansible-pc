fcb0e5a2c6b674c1c05b6333e54c17e62fd29ec7
Zhong Jianxin <azuwis@gmail.com>
2019-11-29 15:10:09 +0800

    torrent_ratio: Check if shared dict empty before loading from file

--- roles/torrent_ratio/files/init.lua
+++ roles/torrent_ratio/files/init.lua
@@ -1,13 +1,14 @@
 local torrent = ngx.shared.torrent
 
-local file = io.open('/var/lib/nginx/torrent_ratio')
-if file ~= nil then
-  for line in file:lines() do
-    local info_hash, info = string.match(line, '^([0-9a-f]+) ([0-9,]+)$')
-    if info_hash ~= nil and info ~= nil then
-      torrent:set(info_hash, info, 10800)
+if #(torrent:get_keys(1)) == 0 then
+  local file = io.open('/var/lib/nginx/torrent_ratio')
+  if file ~= nil then
+    for line in file:lines() do
+      local info_hash, info = string.match(line, '^([0-9a-f]+) ([0-9,]+)$')
+      if info_hash ~= nil and info ~= nil then
+        torrent:set(info_hash, info, 10800)
+      end
     end
+    file:close()
   end
-  file:close()
 end
-
