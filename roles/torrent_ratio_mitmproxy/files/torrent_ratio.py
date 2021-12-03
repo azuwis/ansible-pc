742ec159d1f492104e99b6ce746d30fb19cf6c7f
Zhong Jianxin <azuwis@gmail.com>
2021-12-04 00:59:22 +0800

    torrent_ratio_mitmproxy: Update follow go version

--- roles/torrent_ratio_mitmproxy/files/torrent_ratio.py
+++ roles/torrent_ratio_mitmproxy/files/torrent_ratio.py
@@ -59,43 +59,49 @@ class TorrentRatio:
             return '%.1f%s' % (num, 'T')
 
         query = flow.request.query
-        info_hash = query['info_hash'].encode('utf-8', 'surrogateescape').hex()
-        uploaded = int(query['uploaded'])
-        downloaded = int(query['downloaded'])
-        host = flow.request.pretty_host
+        req_info = {}
+        req_info['info_hash'] = query['info_hash'].encode('utf-8', 'surrogateescape').hex()
+        req_info['uploaded'] = int(query['uploaded'])
+        req_info['downloaded'] = int(query['downloaded'])
+        req_info['host'] = flow.request.pretty_host
         setting = self.setting['default']
-        if host in self.setting:
-            setting = self.setting[host]
+        if req_info['host'] in self.setting:
+            setting = self.setting[req_info['host']]
         if setting['peer_id'] != '':
-            query['peer_id'] = setting['peer_id']
+            query['peer_id'] = '-{}-'.format(setting['peer_id'])
         if setting['port'] > 0 and setting['port'] < 65536:
             query['port'] = setting['port']
-        epoch = time.time()
-        report_uploaded = uploaded
-        incomplete = -3
-        info = self.con.execute('''SELECT * FROM torrent WHERE info_hash=?''', (info_hash,)).fetchone()
-        if ('event' not in query or query['event'] != 'started') and info:
-            delta_uploaded = uploaded - info['uploaded']
-            delta_downloaded = downloaded - info['downloaded']
-            delta_epoch = epoch - info['epoch']
-            if delta_uploaded >= 0 and delta_downloaded >= 0 and delta_epoch <= 10800:
-                report_uploaded = info['report_uploaded']
-                report_uploaded += delta_uploaded
-                incomplete = info['incomplete']
-                if incomplete >= 1:
-                    report_uploaded += math.floor(delta_uploaded * random.uniform(*setting['uploaded']))
-                    report_uploaded += math.floor(delta_downloaded * random.uniform(*setting['downloaded']))
-                    percent = min(setting['percent_min'] + (incomplete - 1) * setting['percent_step'], setting['percent_max'])
-                    if random.random() < percent:
-                        report_uploaded += math.floor(delta_epoch * setting['speed'] * random.random())
-                query['uploaded'] = report_uploaded
+        req_info['epoch'] = time.time()
+        req_info['report_uploaded'] = req_info['uploaded']
+        req_info['incomplete'] = -2
+        init = ''
+        prev_req_info = self.con.execute('''SELECT * FROM torrent WHERE info_hash=?''', (info_hash,)).fetchone()
+        if not prev_req_info:
+            init = 'init, '
+        else:
+            if ('event' not in query or query['event'] != 'started'):
+                delta_uploaded = req_info['uploaded'] - prev_req_info['uploaded']
+                delta_downloaded = req_info['downloaded'] - prev_req_info['downloaded']
+                delta_epoch = req_info['epoch'] - prev_req_info['epoch']
+                if delta_uploaded >= 0 and delta_downloaded >= 0 and delta_epoch <= 10800:
+                    req_info['report_uploaded'] = prev_req_info['report_uploaded']
+                    req_info['report_uploaded'] += delta_uploaded
+                    if prev_req_info['incomplete'] >= 1:
+                        req_info['report_uploaded'] += math.floor(delta_uploaded * random.uniform(*setting['uploaded']))
+                        req_info['report_uploaded'] += math.floor(delta_downloaded * random.uniform(*setting['downloaded']))
+                        percent = min(setting['percent_min'] + (prev_req_info['incomplete'] - 1) * setting['percent_step'], setting['percent_max'])
+                        if random.random() < percent:
+                            req_info['report_uploaded'] += math.floor(delta_epoch * setting['speed'] * random.random())
+                    query['uploaded'] = req_info['report_uploaded']
         self.con.execute('''REPLACE INTO
         torrent(info_hash, host, report_uploaded, uploaded, downloaded, epoch, incomplete)
         values (?, ?, ?, ?, ?, ?, ?)''',
-                         (info_hash, host, report_uploaded, uploaded, downloaded, epoch, -2))
+                         (req_info['info_hash'], req_info['host'], req_info['report_uploaded'], req_info['uploaded'],
+                          req_info['downloaded'], req_info['epoch'], req_info['incomplete']))
         self.con.commit()
-        ctx.log.warn('%s up: %s/%s, down: %s, incomplete: %s, host: %s' %
-                     (info_hash, format(report_uploaded), format(uploaded), format(downloaded), incomplete, host))
+        ctx.log.warn('%s %sup: %s/%s, down: %s, host: %s, epoch: %s' %
+                     (req_info['info_hash'], init, format(req_info['report_uploaded']), format(req_info['uploaded']),
+                      format(req_info['downloaded']), req_info['host'], req_info['epoch']))
 
     def response(self, flow):
         pattern = re.compile('10:incompletei(\d+)e') # noqa
@@ -110,7 +116,7 @@ class TorrentRatio:
             self.con.execute('''UPDATE torrent SET incomplete=? WHERE info_hash=?''',
                              (incomplete, info_hash))
             self.con.commit()
-            ctx.log.warn('%s incomplete: %s' % (info_hash, incomplete))
+            ctx.log.warn('%s incomplete: %d' % (info_hash, incomplete))
 
 
 addons = [
